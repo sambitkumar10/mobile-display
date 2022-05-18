58716d20b985d49b06bf927ff5b4a53e0aec07c7
sambitkumar10 <100463831+sambitkumar10@users.noreply.github.com>
2022-05-19 00:13:41 +0530

    Add files via upload

---
+++ src/Components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div style={{textAlign:"justify"}}>
+        <div>
+        <h1>Mobile Operating System</h1>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+        </div>
+
+        <div>
+        <h1>Mobile Manufacturers</h1>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+        </div>
+    </div>
+  )
+}
+
+export default Home
